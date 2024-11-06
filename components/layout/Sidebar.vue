4ae37d037d3dc69e181e936a73e3d2da77d46622
Boss <crazyhistorian2000@yandex.ru>
2024-11-06 07:51:29 +0300

    add ui libs

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-	<aside class="w-full h-full px-5 py-8 relativebg-sidebar">
+	<aside class="w-full h-full px-5 py-8 relative bg-cyan-900">
 		<nuxt-link to="/" class="mb-10 block">
 			<NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
 		</nuxt-link>
+		<button class="absolute top-2 right-3 transition-colors">
+			<Icon name="line-md:logout" size="20" />
+		</button>
+		<LayoutMenu />
 	</aside>
 </template>
---
+++ components/layout/menu.data.ts
@@ -0,0 +1,23 @@
+export interface IMenuItem {
+	name: string
+	url: string
+	icon: string
+}
+
+export const MENU_DATA: IMenuItem[] = [
+	{
+		name: 'Home',
+		url: '/',
+		icon: 'radix-icons:dashboard',
+	},
+	{
+		name: 'Home',
+		url: '/',
+		icon: 'radix-icons:dashboard',
+	},
+	{
+		name: 'Home',
+		url: '/',
+		icon: 'radix-icons:dashboard',
+	},
+]
