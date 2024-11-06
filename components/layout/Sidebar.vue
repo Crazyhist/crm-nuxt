f261390c988c3f06dd551e4503c7d631eb2ff3f9
Boss <crazyhistorian2000@yandex.ru>
2024-11-06 03:49:21 +0300

    add shadcn-nuxt

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-	<aside>
-		<nuxt-link to="/">
-			<NuxtImg src="/logo.svg" alt="Logo" width="100px" />
+	<aside class="w-full h-full px-5 py-8 relativebg-sidebar">
+		<nuxt-link to="/" class="mb-10 block">
+			<NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
 		</nuxt-link>
 	</aside>
 </template>
