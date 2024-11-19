0f75e05785a94428f0deac0d159242e45cb0d803
Boss <crazyhistorian2000@yandex.ru>
2024-11-19 18:37:53 +0300

    add loader

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,11 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useAuthStore, useIsLoadingStore } from '~/store/auth.store'
+
+const isLoadingStore = useIsLoadingStore()
+const store = useAuthStore()
+const router = useRouter()
+
+const logout = async () => {
+	isLoadingStore.set(true)
+	await account
+}
+</script>
 
 <template>
 	<aside class="w-full h-full px-5 py-8 relative bg-cyan-900">
 		<nuxt-link to="/" class="mb-10 block">
 			<NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
 		</nuxt-link>
-		<button class="absolute top-2 right-3 transition-colors">
+		<button class="absolute top-2 right-3 transition-colors" @click="logout">
 			<Icon name="line-md:logout" size="20" />
 		</button>
 		<LayoutMenu />
